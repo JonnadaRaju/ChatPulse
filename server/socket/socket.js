bba76809455ddc4596ae40739e05dad7315de49a
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-03-14 13:35:05 +0530

    Track online users and broadcast user:online/:offline events

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -4,6 +4,8 @@ const setupUserHandlers = require('./handlers/user.handler');
 const setupRoomHandlers = require('./handlers/room.handler');
 const setupMessageHandlers = require('./handlers/message.handler');
 
+const onlineUsers = new Map();
+
 const initializeSocket = (io) => {
   io.use(async (socket, next) => {
     try {
@@ -30,17 +32,52 @@ const initializeSocket = (io) => {
   io.on('connection', (socket) => {
     console.log(`User connected: ${socket.user.username}`);
 
+    onlineUsers.set(socket.user._id.toString(), {
+      userId: socket.user._id,
+      username: socket.user.username,
+      socketId: socket.id
+    });
+
     socket.emit('auth:success', {
       userId: socket.user._id,
       username: socket.user.username
+    });
+
+    socket.emit('users:online', {
+      users: Array.from(onlineUsers.values()).map(u => ({
+        userId: u.userId,
+        username: u.username
+      }))
+    });
+
+    io.emit('user:online', {
+      userId: socket.user._id,
+      username: socket.user.username,
+      isOnline: true
     });
 
     setupUserHandlers(io, socket, socket.user);
     setupRoomHandlers(io, socket, socket.user);
     setupMessageHandlers(io, socket, socket.user);
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async () => {
       console.log(`User disconnected: ${socket.user.username}`);
+      onlineUsers.delete(socket.user._id.toString());
+      
+      try {
+        await User.findByIdAndUpdate(socket.user._id, {
+          isOnline: false,
+          lastSeen: new Date()
+        });
+        io.emit('user:offline', {
+          userId: socket.user._id,
+          username: socket.user.username,
+          isOnline: false,
+          lastSeen: new Date()
+        });
+      } catch (error) {
+        console.error('Error updating offline status:', error);
+      }
     });
   });
 };
